2f2a835557a4b102a8a3eef509da68025e9d5145
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 19:14:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop(array){
   for (let i=0;i<25,i++){
-    array.push(`I am ${i} strange loop.`)
+    if (i=1) {
+      array.push('I am 1 strange loop.')
+    }else {
+      array[i] = `I am ${i} strange loops.}
+    }
   }
   return array
 }
